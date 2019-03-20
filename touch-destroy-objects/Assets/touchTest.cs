2b7fb1f083160985f732c61024b97aa8da523068
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-20 14:01:18 +0900

    done

---
+++ touch-destroy-objects/Assets/touchTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchTest : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 || Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Debug.Log("Hit Sth");
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
